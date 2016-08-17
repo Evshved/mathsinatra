7c83680037a64770faaa62fd4008ed91ff914eb4
Shved <snuggleshved@gmail.com>
2016-08-17 17:25:23 +0300

    Clear spec_helper.rb. Add 1 test for linear equation. Add comments in tests

--- spec/lib/linear_equation_spec.rb
+++ spec/lib/linear_equation_spec.rb
@@ -1,1 +1,19 @@
-require 'spec_helper'
+# # Test linear
+# equation = Equation.new
+# equation.linear(0,1) => []
+# equation.linear(1,1) => [1]
+
+require_relative '../spec_helper'
+#require_relative '../spec_helper'
+RSpec.describe LinearEquation  do
+  describe '#roots' do
+    subject { described_class.new(k, b) }
+
+    context 'without roots' do
+      let(:k) { 0 }
+      let(:b) { 5 }
+
+      specify { expect(subject.roots).to be_empty }
+    end
+  end
+end
