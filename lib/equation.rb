4c4337cff9a07be44f3461d2284118a89cf4133e
Shved <snuggleshved@gmail.com>
2016-08-17 17:11:21 +0300

    New code for solving equations

--- lib/equation.rb
+++ lib/equation.rb
@@ -1,4 +1,11 @@
 class Equation
+  def linear(k,b)
+    @linear_output = LinearEquation.new(k,b)
+    p @linear_output.roots
+  end
 
-
+  def quadratic(a,b,c)
+    @quadratic_output = QuadraticEquation.new(a,b,c)
+    p @quadratic_output.roots
+  end
 end
