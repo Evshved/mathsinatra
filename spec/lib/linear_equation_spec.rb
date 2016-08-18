63fc9421d9a7b6e988e10201be01610e0e94b693
Shved <snuggleshved@gmail.com>
2016-08-18 15:55:08 +0300

    Update Tests

--- spec/lib/linear_equation_spec.rb
+++ spec/lib/linear_equation_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe LinearEquation  do
       let(:k) { 0 }
       let(:b) { 5 }
 
-      specify { expect(subject.roots).to be_empty }
+      specify { expect(subject.roots).to eq('Answer: no roots') }
     end
 
     context 'with root = 1' do
       let(:k) { 1 }
       let(:b) { 1 }
 
-      specify { expect(subject.roots).to eq(-1) }
+      specify { expect(subject.roots).to eq('Answer: -1') }
     end
   end
 end
