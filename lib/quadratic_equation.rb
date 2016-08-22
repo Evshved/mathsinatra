f5bd04f43abf4aeeb9132c7eb58a26fc3faaf591
Shved <snuggleshved@gmail.com>
2016-08-22 18:45:11 +0300

    Trash comments

--- lib/quadratic_equation.rb
+++ lib/quadratic_equation.rb
@@ -8,17 +8,23 @@ class QuadraticEquation < Equation
   def roots
     return 'Answer: no roots' if discriminant < 0
     if discriminant == 0
-    @answer = [-@b / (2 * @a)]
-    return "Answer: #{@answer}"
+      @answer = ( -@b / (2 * @a) ) # массив не нужно 1 аргумент
+      return "Answer: #{@answer}"
+      # 1 строка
     end
+
     @sqrt = Math.sqrt(discriminant)
     @answer = [-@b + @sqrt, -@b - @sqrt].map { |item| item / (2 * @a) }
     "Answer: #{@answer}"
+    #  1 строка
   end
 
+
+  # метод на квадратный корень
   private
 
   def discriminant
     @discriminant ||= @b**2 - 4 * @a * @c
   end
 end
+#  не надо sqrt, answer
